93f6ed97a740a270102329c42657d90a52328b65
Sean Hung <35648173+seanhung07@users.noreply.github.com>
2023-11-06 11:39:06 -0500

    Starter code for blackjack.sol

--- Smart Contract/blackjack.sol
+++ Smart Contract/blackjack.sol
@@ -1,1 +1,92 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+// Import the Ownable contract from OpenZeppelin to manage ownership
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+// Import SafeMath library to prevent overflows
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+
+// Interface for the RNG Oracle
+interface IRNGOracle {
+    function requestRandomNumber() external returns (bytes32 requestId);
+    function getRandomNumber(bytes32 requestId) external view returns (uint256 randomNumber);
+}
+
+contract Blackjack is Ownable {
+    using SafeMath for uint256;
+
+    IRNGOracle public rngOracle;
+    
+    // Event emitted when a player places a bet
+    event BetPlaced(address indexed player, uint256 amount);
+    
+    // Event emitted when cards are dealt
+    event CardsDealt(address indexed player, uint256[] playerCards, uint256 dealerCard);
+
+    // Event emitted for player actions
+    event PlayerAction(address indexed player, string action);
+
+    // Event emitted when the player's chips are swapped for ETH
+    event ChipsSwappedForEth(address indexed player, uint256 chipAmount);
+
+    // Event emitted when ETH is swapped for chips
+    event EthSwappedForChips(address indexed player, uint256 ethAmount);
+
+    constructor(IRNGOracle _rngOracle) {
+        rngOracle = _rngOracle;
+    }
+
+    // Function to handle age verification
+    function verifyAge() external {
+        // Logic to verify player's age
+    }
+
+    // Function to handle the swapping of ETH for game chips
+    function swapEthForChips() external payable {
+        // Logic to swap ETH for game chips
+    }
+
+    // Function to handle the swapping of game chips for ETH
+    function swapChipsForEth(uint256 chipAmount) external {
+        // Logic to swap game chips for ETH
+    }
+
+    // Function to place a bet
+    function bet(uint256 amount) external {
+        // Logic for betting
+        emit BetPlaced(msg.sender, amount);
+    }
+
+    // Function to deal cards
+    function dealCards() external {
+        // Logic to deal cards
+    }
+
+    // Player action functions
+    function hit() external {
+        // Logic for "hit" action
+        emit PlayerAction(msg.sender, "hit");
+    }
+
+    function stand() external {
+        // Logic for "stand" action
+        emit PlayerAction(msg.sender, "stand");
+    }
+
+    function double() external {
+        // Logic for "double" action
+        emit PlayerAction(msg.sender, "double");
+    }
+
+    function split() external {
+        // Logic for "split" action
+        emit PlayerAction(msg.sender, "split");
+    }
+
+    function surrender() external {
+        // Logic for "surrender" action
+        emit PlayerAction(msg.sender, "surrender");
+    }
+}
 
