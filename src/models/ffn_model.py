c6147ab42463f2de5dca208d9bfe80c907d7f593
mnuryga <mnuryga@gmail.com>
2023-11-06 14:24:46 -0500

    Added .gitignore and preliminary ML models.

---
+++ src/models/ffn_model.py
@@ -0,0 +1,59 @@
+'''
+PyTorch implementation of a simple FFN model for aggregation of inputs
+
+Author: urygam | Matt Uryga
+'''
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+import numpy as np
+import sys
+
+class FFN_Model(nn.Module):
+	def __init__(self, in_dim, hidden_dims, out_dim):
+		'''
+		in_dim: dimensionality of input tensor
+		hidden_dims: list of dimensionalities of hidden layers, None if no hidden layers
+		out_dim: dimensionality of output
+		'''
+
+		super().__init__()
+
+		# if no hidden layers create single layer
+		if hidden_dims is None or len(hidden_dims) == 0:
+			layers = [nn.Linear(in_dim, out_dim)]
+
+		# otherwise create layers with hidden_dims dimensions
+		else:
+			# input layer
+			layers = [nn.Linear(in_dim, hidden_dims[0])]
+
+			# hidden layers
+			for i in range(len(hidden_dims)-1):
+				layers.append(nn.Linear(hidden_dims[i], hidden_dims[i+1]))
+
+			# final layer
+			layers.append(nn.Linear(hidden_dims[-1], out_dim))
+
+		# create ModuleList
+		self.layers = nn.ModuleList(layers)
+
+	def forward(self, x):
+		'''
+		forward pass for FFN
+
+		x: input tensor of dimensionality in_dim
+		'''
+
+		# feed input through all but last layer
+		# using ReLU as activation function
+		for l in self.layers[:-1]:
+			x = nn.ReLU(l(x))
+
+		# last layer use sigmoid as activation function
+		# output should be scalar from 0 to 1
+		x = self.layers[-1](x)
+
+		return torch.sigmoid(x)
---
+++ src/models/odds_tailoring_model.py
@@ -0,0 +1,57 @@
+'''
+PyTorch implementation of a LSTM model for odds tailoring
+
+Author: urygam | Matt Uryga
+'''
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from FFN_Model import FFN_Model
+
+import numpy as np
+import sys
+
+class Odds_Tailoring_Model(nn.Module):
+	def __init__(self, in_len, in_dim, hidden_dims, out_dim, num_layers=2, batch_first=True):
+		'''
+		in_len: length of input tensor
+		in_dim: dimensionality of input tensor
+		hidden_dims: dimensionality of ffn hidden layers
+		out_dim: dimensionality of output
+		num_layers=2: number of stacked LSTM layers
+		batch_first=True: swaps axes such that batches are first axis
+		'''
+		super().__init__()
+		self.in_len = in_len
+
+		# create input aggregator -> converts all inputs to single scalar [0, 1]
+		self.input_aggregator = FFN_Model(in_dim, hidden_dims, 1)
+
+		# create lstm, will utilize in_dim outputs from ffn
+		self.lstm = nn.LSTM(in_len, 4*out_dim, num_layers=num_layers, batch_first=batch_first)
+
+		# create ffn to process lstm output into scalar
+		self.output_aggregator = FFN_Model(in_len, [in_len//4], 1)
+
+	def forward(self, x):
+		'''
+		forward pass for cheat detection model
+
+		x: input tensor of dimensionality in_dim with batch size in_len
+		'''
+		x = self.input_aggregator(x)
+
+		# reshape ffn output for lstm input
+		x = x.reshape(self.in_len,)
+
+		# feed into lstm
+		_, (hn, _) = self.lstm(x)
+
+		# extract output from lstm
+		x = hn[0, :, :]
+
+		# feed into output aggregation ffn
+		x = self.output_aggregator(x)
+
+		return x
