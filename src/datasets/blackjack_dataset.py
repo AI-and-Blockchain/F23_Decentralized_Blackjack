39059e17c386a632e53f07ffe3f6ab5b4fb873b6
mnuryga <mnuryga@gmail.com>
2023-11-16 17:45:00 -0500

    Initial implementation of data generation.

--- src/datasets/blackjack_dataset.py
+++ src/datasets/blackjack_dataset.py
@@ -1,3 +1,38 @@
 '''
-placeholder file
+Custom Dataset class from training models
+
+Author: Matthew Uryga
 '''
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils.data import Dataset
+
+import numpy as np
+import sys
+import pickle as pkl
+
+class Blackjack_Dataset(Dataset):
+	def __init__(self, data_path):
+		'''
+		data_path: relative path to data pickle file
+		'''
+		with open('data_path', 'rb') as f:
+			raw_data = pickle.load(f)
+			self.X = raw_data[:, :-1]
+			self.y = raw_data[:, -1]
+
+	def __len__(self):
+		'''
+		len function
+		'''
+		return len(self.X)
+
+	def __getitem__(self, idx):
+		'''
+		idx: index to get values at
+
+		will return features and labels as a tuple
+		'''
+		return self.X[idx], self.y[idx]
