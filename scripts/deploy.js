947d397632d293d88271e824d69c359a4fddac04
Isaac_the_Man <steven97102@gmail.com>
2023-11-15 19:57:03 -0500

    feat: deploy script.

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -2,37 +2,56 @@
 // Please make sure to compile all files before running this script.
 // And use Right click -> "Run" from context menu of the file to run the script. Shortcut: Ctrl+Shift+S
 
-import { deploy } from './web3-lib'
+import 'web3-eth'
 
+async function deploy(name, args = []) {
+    // Note that the script needs the ABI which is generated from the compilation artifact.
+    const metadata = JSON.parse(await remix.call('fileManager', 'getFile', `contracts/artifacts/${name}.json`))
+    const accounts = await web3.eth.getAccounts()
+
+    let contract = new web3.eth.Contract(metadata.abi)
+
+    contract = contract.deploy({
+      data: metadata.data.bytecode.object,
+      arguments: args
+    })
+
+    newContractInstance = await contract.send({
+      from: accounts[0],
+      gas: 3000000,
+      gasPrice: '30000000000'
+    })
+
+    return newContractInstance
+}
+
+// contract deployment logic
 (async () => {
-    // deploy BJT contract
-    const BJTAddress = await (async () => {
-        try {
-            const result = await deploy('BJT', [])
-            console.log(`Deploying BJT to address: ${result.address}`)
-            return result.address
-        } catch (e) {
-            console.log(e.message)
-        }
-    })()
-    // deploy Authenticator contract
-    const AuthAddress = await (async () => {
-        try {
-            const result = await deploy('BJT', [])
-            console.log(`Deploying Authenticator to address: ${result.address}`)
-            return result.address
-        } catch (e) {
-            console.log(e.message)
-        }
-    })()
-    // deploy Cage contract
-    const CageAddress = await (async () => {
-        try {
-            const result = await deploy('Cage', [BJTAddress, AuthAddress])
-            console.log(`Deploying Cage to address: ${result.address}`)
-            return result.address
-        } catch (e) {
-            console.log(e.message)
-        }
-    })()
+    const accounts = await web3.eth.getAccounts()
+
+    const ageVerifierAddr = accounts[0]
+    const mlBotAddr = accounts[0]
+
+    try {
+        console.log('deploying contract BlackJackToken...')
+        const BJTContract = await deploy('BlackJackToken')
+        console.log(BJTContract.options.address)
+
+        console.log('deploying contract Authenticator...')
+        const AuthContract = await deploy('Authenticator', [ageVerifierAddr, mlBotAddr])
+        console.log(AuthContract.options.address)
+
+        console.log('deploying contract Cage...')
+        const CageContract = await deploy('Cage', [BJTContract.options.address, AuthContract.options.address])
+        console.log(CageContract.options.address)
+
+        // grant Cage module MINTER_ROLE
+        console.log('grant contract Cage the MINTER role...')
+        await BJTContract.methods.grantMinterRole(CageContract.options.address).send({from: accounts[0]})
+
+        console.log('DONE')
+
+    } catch (e) {
+        console.log(e.message)
+    }
 })()
